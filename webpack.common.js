b07eb3b36ab8b65627c451a65747fd0a4e50c271
kurnia123 <rizkykurnia219@gmail.com>
2020-06-14 22:41:17 +0700

    commit pertama

---
+++ webpack.common.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+let data = ["nav","index","standingTeam","detail"];
+
+let generateHtmlPlugin = function() {
+    let tmp = []
+    data.forEach(item => {
+        
+        if(item === "index") {
+            tmp.push(
+                new HtmlWebpackPlugin({
+                    filename: `index.html`,
+                    template: `./src/index.html`,
+                    chunks: [`${item}`]
+                })
+            )
+        } else if(item === "nav") {
+            tmp.push(
+                new HtmlWebpackPlugin({
+                    filename: `${item}.html`,
+                    template: `./src/${item}.html`,
+                })
+            )
+        } else {
+            tmp.push(
+                new HtmlWebpackPlugin({
+                    filename: `${item}/${item}.html`,
+                    template: `./src/template/${item}/${item}.html`,
+                    chunks: [`${item}`]
+                })
+            )
+        }
+
+    });
+    return tmp;
+}
+
+module.exports = {
+    entry: {
+        index: "./src/script/view/index/index.js",
+        detail: "./src/script/view/detail/detail.js"
+    },
+    output: {
+        filename: "[name]/[name]_bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    }
+                ]
+            },
+            {
+                test:/\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000,
+                        name: 'assets/[name].[ext]'
+                    }
+                }]
+                
+            }
+
+        ]
+    },
+    plugins: [
+        
+    ].concat(generateHtmlPlugin())
+}
